1f935984a5ed743fb10da3629b95ebabe14790d0
Your Name <Your email>
2016-05-17 18:43:48 -0700

    Added view controllers files

---
+++ oweUone/FavorFeedViewController.swift
@@ -0,0 +1,14 @@
+//
+//  FavorFeedViewController.swift
+//  oweUone
+//
+//  Created by Xiaowen Feng on 5/17/16.
+//  Copyright © 2016 Xiaowen Feng, Peter Freschi, Quynh Huynh. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class FavorFeedViewController: UIViewController {
+
+}
---
+++ oweUone/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  oweUone
+//
+//  Created by Xiaowen Feng on 5/17/16.
+//  Copyright © 2016 Xiaowen Feng, Peter Freschi, Quynh Huynh. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class LoginViewController: UIViewController {
+
+}
